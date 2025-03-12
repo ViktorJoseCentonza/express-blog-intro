f29d90d970ee4b3ff2932e5ad6e069da142ae9a3
ViktorJoseCentonza <viktorjose02@gmail.com>
2025-03-12 14:38:36 +0100

    add first route api response

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`my-blog listening on port http://localhost:${port}`);
+})
 console.log("serverside running");
+
+
+app.get('/', (req, res) => {
+    res.send("server del mio blog")
+})
